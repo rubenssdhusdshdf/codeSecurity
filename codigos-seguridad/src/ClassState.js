b4cbadf03504bfa88b1b389ceee2710cb9d4b5c2
Ruben <rubenvegajusto@gmail.com>
2023-08-06 12:40:58 +0100

    useEffect to do a validation showing loading state to the user

--- codigos-seguridad/src/ClassState.js
+++ codigos-seguridad/src/ClassState.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class ClassState extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            error: true,
+        };
+    }
     render() {
         return (
             <div>
-                <h2>Eliminate ClassState</h2>
+                <h2>Eliminate {this.props.name} </h2>
 
                 <p>Please, write here the security code</p>
+
+                {this.state.error && (
+                    <p>Error: the code is incorrect</p>
+                )}
+
                 <input placeholder="security code"/>
-                <button>Check</button>
+                <button
+                    onClick={ () => 
+                        this.setState( prevState => ({ error: !prevState.error }) )
+                    }
+                >Check</button>
             </div>
         )
     }
--- codigos-seguridad/src/UseState.js
+++ codigos-seguridad/src/UseState.js
@@ -1,13 +1,44 @@
 import React from 'react';
 
-function UseState() {
+function UseState( { name} ) {
+
+    const [error, setError] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
+ 
+    React.useEffect(() => {
+        console.log("Empezando el efecto")
+
+        if (!!loading) {
+            setTimeout(() => {
+                console.log("Haciendo la validacion")
+    
+                setLoading(false);
+    
+                console.log("terminando la validacion")
+            }, 3000)
+        }
+
+        console.log("Terminando el efecto")
+    }, [loading]);
     return (
         <div>
-            <h2>Eliminate UseState</h2>
+            <h2>Eliminate {name} </h2>
+
+
+            
 
             <p>Please, write here the security code</p>
+
+            {error && (
+                <p>Error: the code is incorrect</p>
+            )}
+            {loading && (
+                <p>Loading... </p>
+            )}
             <input placeholder="security code"/>
-            <button>Check</button>
+            <button
+                onClick={() => setLoading(true)} //Or onClick={() => setError(prevState => !prevState )}
+            >Check</button>
         </div>
     )
 }
